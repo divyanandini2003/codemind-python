d139bb93b0d068e0144285c497ff38abfd26472b
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-05-16 22:54:44 +0530

    n=int(input()) a=n%10 n=n//10 b=n%10 n=n//10 c=n%10 n=n//10 d=n%10 n=n//10 if(d==6):     d=9 elif(c==6):     c=9 elif(b==6):     b=9 elif(a==6):     a=9 print(d,end='') print(c,end='') print(b,end='') print(a,end='')

---
+++ Maximum_69_Number.py
@@ -0,0 +1,22 @@
+n=int(input())
+a=n%10
+n=n//10
+b=n%10
+n=n//10
+c=n%10
+n=n//10
+d=n%10
+n=n//10
+if(d==6):
+    d=9
+elif(c==6):
+    c=9
+elif(b==6):
+    b=9
+elif(a==6):
+    a=9
+print(d,end='')
+print(c,end='')
+print(b,end='')
+print(a,end='')
+    
