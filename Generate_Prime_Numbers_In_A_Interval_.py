70ff812e0e378d8706bee8a0a40ebbd806f5838c
divyanandini2003 <97425770+divyanandini2003@users.noreply.github.com>
2022-06-05 23:51:46 +0530

    lower_value = int(input()) upper_value = int(input())
    for number in range (lower_value, upper_value + 1):  
        if number > 1:  
            for i in range (2, number):  
                if (number % i) == 0:  
                    break  
            else:  
                print (number)

---
+++ Generate_Prime_Numbers_In_A_Interval_.py
@@ -0,0 +1,10 @@
+lower_value = int(input()) 
+upper_value = int(input())  
+ 
+for number in range (lower_value, upper_value + 1):  
+    if number > 1:  
+        for i in range (2, number):  
+            if (number % i) == 0:  
+                break  
+        else:  
+            print (number)
